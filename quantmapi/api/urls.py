49cab029bc410056d02f9473138fc0a4d8427cab
turmanauli <bsovevo@gmail.com>
2024-07-15 23:45:41 +0400

    [fix] fixed API list filters for ticker and timeframe arguments

--- quantmapi/api/urls.py
+++ quantmapi/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('api/', views.ChartDataListCreate.as_view(), name='chart-data-view'),
     path('api/<int:pk>', views.ChartDataRetrieveUpdateDestroy.as_view(), name='chart-data-update'),
-    path('api/<str:ticker>', views.ChartDataListCreate.as_view(), name='ticker-data-view'),
-    path('api/<str:ticker>/<int:timeframe>', views.ChartDataListCreate.as_view(), name='ticker-data-tf-view'),
+    path('api/<str:ticker>', views.ChartDataListTicker.as_view(), name='ticker-data-view'),
+    path('api/<str:ticker>/<int:timeframe>', views.ChartDataListTickerTimeframe.as_view(), name='ticker-data-tf-view'),
 ]
--- quantmapi/api/views.py
+++ quantmapi/api/views.py
@@ -1,26 +1,48 @@
+from urllib import request, response
 from django.shortcuts import render
 from rest_framework import generics
 from .models import ChartData
 from .serializers import ChartDataSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 class ChartDataListCreate(generics.ListCreateAPIView):
     queryset = ChartData.objects.all()
     serializer_class = ChartDataSerializer
 
-
-class ChartDataListCreate(generics.ListCreateAPIView):
-    queryset = ChartData.objects.all()
-    serializer_class = ChartDataSerializer
-    lookup_field = 'ticker'
-
-class ChartDataListCreate(generics.ListCreateAPIView):
-    queryset = ChartData.objects.all()
-    serializer_class = ChartDataSerializer
-    lookup_fields = ['ticker', 'timeframe']
-
-
+# display and modify/delete entry by entry id
 class ChartDataRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = ChartData.objects.all()
     serializer_class = ChartDataSerializer
     lookup_field = 'pk'
+
+
+# list entries by ticker
+class ChartDataListTicker(generics.GenericAPIView):
+    queryset = ChartData.objects.all()
+    serializer_class = ChartDataSerializer
+
+    def get_queryset(self):
+        ticker = self.kwargs['ticker']
+        return ChartData.objects.filter(ticker=ticker)
+
+    def get(self, request, ticker):
+        queryset = self.get_queryset()
+        serializer = ChartDataSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+# list entries by ticker and timeframe
+class ChartDataListTickerTimeframe(generics.ListAPIView):
+    queryset = ChartData.objects.all()
+    serializer_class = ChartDataSerializer
+    
+    def get_queryset(self):
+        ticker = self.kwargs['ticker']
+        timeframe = self.kwargs['timeframe']
+        return ChartData.objects.filter(ticker=ticker, timeframe=timeframe)
+
+    def get(self, request, ticker, timeframe):
+        queryset = self.get_queryset()
+        serializer = ChartDataSerializer(queryset, many=True)
+        return Response(serializer.data)
